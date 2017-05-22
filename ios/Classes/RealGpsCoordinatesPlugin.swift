2122476df860e190ac7099f8af748ca7177eee20
Gustavo Parreira <gustashminecraft@gmail.com>
2017-05-22 10:52:48 +0100

    Getting the actual location from the device and asking for permission.

--- ios/Classes/RealGpsCoordinatesPlugin.swift
+++ ios/Classes/RealGpsCoordinatesPlugin.swift
@@ -8,9 +8,13 @@
 
 import Foundation
 import UIKit
+import CoreLocation
 
-public class RealGpsCoordinatesPlugin: NSObject, FlutterPlugin {
+public class RealGpsCoordinatesPlugin: NSObject, FlutterPlugin, CLLocationManagerDelegate {
     static let CHANNEL_NAME: String = "gustash.flutter.plugins/gps_coordinates";
+    var manager: CLLocationManager!;
+    var coordinates: NSDictionary = [:];
+    var result: FlutterResult? = nil;
     
     public static func register(with registrar: FlutterPluginRegistrar) {
         let channel = FlutterMethodChannel(name: CHANNEL_NAME, binaryMessenger: registrar.messenger());
@@ -20,12 +24,43 @@ public class RealGpsCoordinatesPlugin: NSObject, FlutterPlugin {
     
     public func handle(_ call: FlutterMethodCall, result: @escaping FlutterResult) {
         if (call.method == "getGPSCoordinates") {
-            var coordinates: Dictionary<String, Double> = Dictionary();
-            coordinates["lat"]  = 25.0;
-            coordinates["long"] = 25.0;
-            result(coordinates as NSDictionary);
+            self.result = result;
+            
+            if CLLocationManager.locationServicesEnabled() {
+                manager = CLLocationManager();
+                manager.requestAlwaysAuthorization();
+                manager.requestWhenInUseAuthorization();
+                manager.delegate = self;
+                manager.desiredAccuracy = kCLLocationAccuracyBest;
+                manager.startUpdatingLocation();
+            }
+            
+            //var coordinates: Dictionary<String, Double> = Dictionary();
+            //coordinates["lat"]  = 25.0;
+            //coordinates["long"] = 25.0;
+            //result(coordinates as NSDictionary);
         } else {
             result(FlutterMethodNotImplemented);
         }
+    }
+    
+    public func locationManager(_ manager: CLLocationManager, didUpdateLocations locations: [CLLocation]) {
+        let userLocation: CLLocation = locations[0];
+        let long                     = userLocation.coordinate.longitude;
+        let lat                      = userLocation.coordinate.latitude;
+        buildDict(lat: lat, long: long);
+        returnResult();
+    }
+    
+    func buildDict(lat: Double, long: Double) {
+        var coordinates: Dictionary<String, Double> = Dictionary();
+        coordinates["lat"]  = lat;
+        coordinates["long"] = long;
+        self.coordinates = coordinates as NSDictionary;
+    }
+    
+    func returnResult() {
+        manager.stopUpdatingLocation();
+        self.result!(self.coordinates);
     }
 }
